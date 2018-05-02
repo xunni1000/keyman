d33046b0e868462bb6666eb5ef078968d1768e0e
Tom Bogle <tom_bogle@sil.org>
2018-05-02 15:08:25 -0400

    [MAC] WIP(808): Initial attempt at changing cursor over hyperlinks
    Apparently because Keyman never becomes the active app (even when it is displaying configuration windows that are key windows), all attempts to get the cursor to change fail. Presumably that's why it's not happening automatically in the first place. In this commit, I was able to add a label that shows the destination URL (not quite correct, since we'd probably want to trim off the keyman?link:url= prefix for links that ae targeting the default browser) when the mouse passes over them. That's probably better than noting.

--- mac/Keyman4MacIM/Keyman4MacIM/KMDownloadKeyboard/KMDownloadKBWindowController.m
+++ mac/Keyman4MacIM/Keyman4MacIM/KMDownloadKeyboard/KMDownloadKBWindowController.m
@@ -11,6 +11,7 @@
 
 @interface KMDownloadKBWindowController ()
 @property (nonatomic, weak) IBOutlet WebView *webView;
+@property (nonatomic, weak) IBOutlet NSTextFieldCell *urlLabel;
 @end
 
 @implementation KMDownloadKBWindowController
@@ -23,11 +24,39 @@
     [super windowDidLoad];
     
     [self.webView setFrameLoadDelegate:(id<WebFrameLoadDelegate>)self];
+    [self.webView setUIDelegate:(id<WebUIDelegate>)self];
     
     NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
     NSString *url = [NSString stringWithFormat:@"https://keyman.com/go/macos/10.0/download-keyboards/?version=%@", version];
     NSLog(@"KMDownloadKBWindowController opening url = %@, version = '%@'", url, version);
     [self.webView.mainFrame loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
+}
+
+BOOL hand = false;
+NSPoint prevMouseLocOverUrl;
+
+- (void)webView:(WebView *)sender mouseDidMoveOverElement:(NSDictionary *)elementInformation modifierFlags:(NSUInteger)modifierFlags {
+    NSArray* keys = [elementInformation objectForKey:WebElementLinkURLKey];
+    if (keys && !hand) {
+        [self.urlLabel setStringValue:[NSString stringWithFormat:@"%@",keys]];
+        hand = YES;
+        // The following DOES NOT work, probably because Keyman is never the active app!
+        [[NSCursor pointingHandCursor] set];
+        prevMouseLocOverUrl = [NSEvent mouseLocation]; // current mouse position
+    }
+    else if (hand) {
+        // This keeps getting called incessantly even if the mouse doesn't move, and if
+        // the mouse is over a link, the keys only come back with the link info the very
+        // first time.
+        NSPoint currentMouseLoc = [NSEvent mouseLocation]; // current mouse position
+        if (currentMouseLoc.x != prevMouseLocOverUrl.x || currentMouseLoc.y != prevMouseLocOverUrl.y) {
+            [self.urlLabel setStringValue:@""];
+            // The following DOES NOT work, probably because Keyman is never the active app!
+            NSLog(@"CURSOR: Arrow");
+            hand = NO;
+            [[NSCursor arrowCursor] set];
+        }
+    }
 }
 
 @end
